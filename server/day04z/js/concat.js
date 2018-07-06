5f1ca560027b19c625820452420d0dd99835a37a
ENGLISH-LIGHT <2319513900@qq.com>
2018-07-06 15:47:12 +0800

    打地鼠点击、积分、倒计时

--- server/day04z/js/concat.js
+++ server/day04z/js/concat.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     // var font1 = document.getElementsByClassName("font");
     // font1[0].innerHTML = "light";
     // font1[0].style.width = "1000px";
@@ -27,7 +27,7 @@
 //         clearInterval(timer);
 //     }
 // },1000);
-    
+
 // }
 
 {
@@ -38,17 +38,56 @@
     var lasttmp = 0;
     // tmp-记录随机数
     var tmp;
-    b.onclick = function(){
+    var dao = document.querySelector(".lasttime");
+    var lt = 10;
+    var click = false;
+    var count = 0;
+    b.onclick = function () {
+        if (click == true) {
+            return;
+        }
+        count = 0;
+        click = true;
         gameDiv.style.visibility = "visible";
-        setInterval(function(){
-           tmp =  Math.floor(Math.random()*9);
-           if(lasttmp !== tmp){
-               imgs[lasttmp].src = "../img/3.png";
-               imgs[tmp].src = "../img/bear.png";
-           }
-           lasttmp = tmp;
-        },1000);
-    }   
+        var timer = null;
+        timer = setInterval(function () {
+
+            // 倒计时
+            if (lt === 0) {
+                lt = 10;
+                count = 0;
+
+                click = false;
+                clearInterval(timer);
+                gameDiv.style.visibility = "hidden";
+                score.innerHTML = "" + count;
+
+            } else {
+                lt--;
+                dao.innerHTML = "" + lt;
+            }
+            tmp = Math.floor(Math.random() * 9);
+            if (lasttmp !== tmp) {
+                imgs[lasttmp].src = "../img/3.png";
+                imgs[tmp].src = "../img/bear.png";
+            }
+            lasttmp = tmp;
+        }, 1000);
+    }
     var a = document.querySelector(".gameDiv");
+    var score = document.querySelector(".score");
+    function beat(obj) {
+        // alert("1")
+        var src = obj.src;
+        // alert(src)
+        var a = src.charAt(src.length - 5);
+        if (a !== "3") {
+            count++;
+            score.innerHTML = "" + count;
+        } else {
+        }
+        // alert(a);
+    }
+
 }
 
