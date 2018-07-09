66bf25c726e3cc0f5afbdeb36cb3cd50171a21bd
ENGLISH-LIGHT <2319513900@qq.com>
2018-07-09 16:22:40 +0800

    第7天下午-轮播图

---
+++ server/day07x/js/concat.js
@@ -0,0 +1,67 @@
+var timer = null;
+var index = 0;
+var nextIndex = 0;
+//滑动函数
+function scrollPlay() {
+    if (index < nextIndex) {
+        $(".imgbox img")
+            .eq(index)
+            .stop(true, true)
+            .animate({
+                "left": "-720px"
+            });
+
+        $(".imgbox img")
+            .eq(nextIndex)
+            .css({
+                "left": "720px"
+            })
+            .stop(true, true)
+            .animate({
+                "left": "0px"
+            });
+        $("li")
+        .eq(index)
+        .addClass("styleFill")
+        .siblings()
+        .removeClass("styleFill");
+
+    } else if (index > nextIndex) {
+        $(".imgbox img")
+            .eq(index)
+            .stop(true, true)
+            .animate({
+                "left": "720px"
+            });
+
+        $(".imgbox img")
+            .eq(nextIndex)
+            .css({
+                "left": "-720px"
+            })
+            .stop(true, true)
+            .animate({
+                "left": "0px"
+            });
+        $("li")
+        .eq(index)
+        .addClass("styleFill")
+        .siblings()
+        .removeClass("styleFill");
+    }
+}
+// 自动滑动
+function autoPlay() {
+    timer = setInterval(function () {
+        nextIndex++;
+        if (nextIndex > 7) {
+            nextIndex = 0;
+        }
+        scrollPlay();
+        index = nextIndex;
+    }, 2000);
+}
+
+autoPlay();
+
+// $("li").toggleClass(".styleFill")
